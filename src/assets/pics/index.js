6393063a0ae060f876baff07d2cb776a8842ae10
Fauzul Ashari <p4ujul@gmail.com>
2022-12-07 01:30:38 +0700

    Added review image for Testimony component.

--- src/assets/pics/index.js
+++ src/assets/pics/index.js
@@ -1,4 +1,7 @@
 import phone1 from "./phone1.png";
 import phone2 from "./phone2.png";
+import review1 from "./review1.png";
+import review2 from "./review2.png";
+import review3 from "./review3.png";
 
-export { phone1, phone2 };
+export { phone1, phone2, review1, review2, review3 };
