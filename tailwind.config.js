28566bbc4fe7d3ef5f6021217ca584ec3939e8e9
M. Mustakim Surya <muhammadmustakimsurya@gmail.com>
2022-12-07 15:54:07 +0700

    screen tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,6 @@ module.exports = {
       screens: {
         tablet: "926px",
         desktop: "1440px",
-        sm: "640px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1280px",
-        2xl: "1536px",
       },
 
       colors: {
