f5e248bb2114a447679d536ce5fcc821dbdfbeaa
Fauzul Ashari <p4ujul@gmail.com>
2022-12-09 16:09:46 +0700

    Edited App.js for testing Dashboard render.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 // import RouteConfig from "./routers/RouteConfig";
-import Navbar from "./components/Dashboard/";
+import Dashboard from "./components/Dashboard/";
 
 const App = () => {
   return (
     <div className="App font-avenir">
-      <Navbar />
+      <Dashboard />
     </div>
   );
 };
