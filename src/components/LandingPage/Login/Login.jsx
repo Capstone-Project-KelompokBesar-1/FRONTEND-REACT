caecc1428d433ff6e045d2820e9d4f7ee468f72b
M. Mustakim Surya <muhammadmustakimsurya@gmail.com>
2022-12-13 21:39:07 +0700

    styling login

--- src/components/LandingPage/Login/Login.jsx
+++ src/components/LandingPage/Login/Login.jsx
@@ -6,37 +6,37 @@ import { logo, phone3 } from "../../../assets/img";
 
 const Login = () => {
   return (
-    <div className="h-screen darkbg pb-28 flex justify-center items-center">
-      <div className="mr-28">
-        <img src={phone3} alt="Mobile App" className="w-[300px]" />
-      </div>
-      <div className="loginpage dark w-[650px] h-[730px] mt-[91px] rounded-[50px]">
-        <form
-          action="submit"
-          className="flex flex-col justify-center items-center"
-        >
-          <h1 className="font-avenirHeavy text-white font-extrabold text-5xl text-center mt-[73px] mb-12">
-            Selamat Datang
-          </h1>
-          <img src={logo} alt="Logo" className="w-[166px] mb-12" />
-          <input
-            type="text"
-            placeholder="Username"
-            className="mb-6 w-[380px] h-14 rounded-lg font-avenirHeavy p-5 border border-primary-500"
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            className=" w-[380px] h-14 rounded-lg font-avenirHeavy p-5 border border-primary-500"
-          />
-          <Link to="/dashboard/main">
-            <button
-              type="submit"
-              className="mt-12 w-[380px] h-14 rounded-lg font-avenirHeavy bg-primary-500 text-white">
-              Masuk
-            </button>
-          </Link>
-        </form>
+    <div className="h-screen bg-gradient-to-tr from-primary-500 to-black flex items-center justify-between px-44">
+      <img src={phone3} alt="Mobile App" className="h-[720px] w-[474px] mr-[46px]" />
+      <div className="darkbg h-[600px] w-[534px] pb-28 flex justify-center items-center align-middle rounded-[50px]">
+        <div className="loginpage dark w-[650px] h-[730px] mt-[91px] rounded-[50px]">
+          <form
+            action="submit"
+            className="flex flex-col justify-center items-center"
+          >
+            <h1 className="font-avenirHeavy text-white font-extrabold text-[46px] text-center mt-[130px] mb-[39px]">
+              Selamat Datang
+            </h1>
+            <img src={logo} alt="Logo" className="w-[130px] mb-[39px]" />
+            <input
+              type="text"
+              placeholder="Username"
+              className="mb-5 w-[312px] h-[47px] rounded-lg font-avenirHeavy p-5 border border-primary-500"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className=" w-[312px] h-[47px] rounded-lg font-avenirHeavy p-5 border border-primary-500"
+            />
+            <Link to="/dashboard/main">
+              <button
+                type="submit"
+                className="mt-[39px] w-[312px] h-[47px] rounded-lg font-avenirHeavy bg-primary-500 text-white">
+                Masuk
+              </button>
+            </Link>
+          </form>
+        </div>
       </div>
     </div>
   );
