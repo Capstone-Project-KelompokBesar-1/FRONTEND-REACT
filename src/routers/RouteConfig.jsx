63c20ed6b84acc18c9e4354e36ca9be5e1de5e0f
M. Mustakim Surya <muhammadmustakimsurya@gmail.com>
2022-12-13 09:20:36 +0700

    Link edit page by edit button

--- src/routers/RouteConfig.jsx
+++ src/routers/RouteConfig.jsx
@@ -8,6 +8,9 @@ import KelasPage from "../pages/DashboardPage/KelasPage/KelasPage";
 import PelatihPage from "../pages/DashboardPage/PelatihPage/PelatihPage";
 import LandingPage from "../pages/LandingPage/LandingPage";
 import EditPelatihPage from "../pages/DashboardPage/PelatihPage/EditPelatihPage";
+import EditAnggotaPage from "../pages/DashboardPage/AnggotaPage/EditAnggotaPage";
+import EditKelasPage from "../pages/DashboardPage/KelasPage/EditKelasPage";
+import EditBookingPage from "../pages/DashboardPage/BookingPage/EditBookingPage";
 
 const RouteConfig = () => {
   return (
@@ -20,9 +23,18 @@ const RouteConfig = () => {
             <Route path="page" element={<PelatihPage />}/>
             <Route path="edit" element={<EditPelatihPage />}/>
           </Route>
-          <Route path="anggota" element={<AnggotaPage />} />
-          <Route path="kelas" element={<KelasPage />} />
-          <Route path="booking" element={<BookingPage />} />
+          <Route path="anggota">
+            <Route path="page" element={<AnggotaPage />}/>
+            <Route path="edit" element={<EditAnggotaPage />}/>
+          </Route>
+          <Route path="kelas">
+            <Route path="page" element={<KelasPage />}/>
+            <Route path="edit" element={<EditKelasPage />}/>
+          </Route>
+          <Route path="booking">
+            <Route path="page" element={<BookingPage />}/>
+            <Route path="edit" element={<EditBookingPage />}/>
+          </Route>
         </Route>
         <Route path="/login" element={<Login />} />
       </Routes>
