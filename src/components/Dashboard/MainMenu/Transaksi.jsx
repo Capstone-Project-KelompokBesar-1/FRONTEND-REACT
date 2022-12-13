29ee9dbeba4dd67f9f70de7c690c599e96ba513c
Fauzul Ashari <p4ujul@gmail.com>
2022-12-14 02:05:46 +0700

    Changed from static data to fetched data from API.

--- src/components/Dashboard/MainMenu/Transaksi.jsx
+++ src/components/Dashboard/MainMenu/Transaksi.jsx
@@ -1,33 +1,27 @@
-const Transaksi = () => {
-  return (
-    <div className="border rounded-2xl h-[500px] overflow-auto">
-      <h3 className="font-avenirHeavy text-xl p-4">Transaksi Terkini</h3>
-      <table className="text-left">
-        <thead className="uppercase bg-primary-700">
-          <tr className="text-white">
-            <th className="py-4 px-6 w-72 mr-12">Nama Kelas</th>
-            <th className="py-4 px-6 w-72 ml-7">Jenis Kelas</th>
-            <th className="py-4 px-6 w-72 ml-7">Tanggal</th>
-            <th className="py-4 px-6 w-72 ml-7">Nama Pemesan</th>
-            <th className="py-4 px-6 w-72 ml-7">Status</th>
-            <th className="py-4 px-6 w-52 ml-7">Harga</th>
-          </tr>
-        </thead>
+const Transaksi = ({ gymClass, classType, date, userName, status, price }) => {
+  const statusCheck = () => {
+    if (status === "tertunda") {
+      return "text-warning";
+    } else if (status === "berhasil") {
+      return "text-success-500";
+    } else if (status === "gagal") {
+      return "text-primary-500";
+    }
+  };
 
-        <tbody className="font-avenirHeavy text-web-dark">
-          <tr>
-            <td className="py-4 px-6">Pompa Tubuh</td>
-            <td className="py-4 px-6">Offline</td>
-            <td className="py-4 px-6">12/12/2021</td>
-            <td className="py-4 px-6">Yusuf</td>
-            <td className="py-4 px-6 font-avenirBlack text-warning">
-              TERTUNDA
-            </td>
-            <td className="py-4 px-6">Rp. 100.000</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+  return (
+    <tbody className="font-avenirHeavy text-web-dark border-t">
+      <tr>
+        <td className="py-4 px-6 capitalize">{gymClass}</td>
+        <td className="py-4 px-6 capitalize">{classType}</td>
+        <td className="py-4 px-6">{date}</td>
+        <td className="py-4 px-6 capitalize">{userName}</td>
+        <td className={`py-4 px-6 font-avenirBlack ${statusCheck()} uppercase`}>
+          {status}
+        </td>
+        <td className="py-4 px-6">Rp. {price}</td>
+      </tr>
+    </tbody>
   );
 };
 
