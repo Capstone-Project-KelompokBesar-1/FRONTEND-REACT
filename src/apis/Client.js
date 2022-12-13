5e4ff3c36d0fe4357d7d8277ebb4e7cd21e6aa74
Fauzul Ashari <p4ujul@gmail.com>
2022-12-14 01:24:05 +0700

    Added Axios instance url & temporary bearer token.

--- src/apis/Client.js
+++ src/apis/Client.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
+const URL = "http://ec2-54-65-17-32.ap-northeast-1.compute.amazonaws.com";
+
 export default axios.create({
-  baseURL: "http://ec2-54-65-17-32.ap-northeast-1.compute.amazonaws.com",
+  baseURL: URL,
   headers: {
     "Content-type": "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaXNfYWRtaW4iOnRydWUsImVtYWlsIjoiYWRtaW5AbWFpbC5jb20iLCJleHAiOjE2NzA5NzcwMDV9.PUQ7M9RvFJwAu4z8q1KyNcOXubPL4HoPUne3cLXYhyw",
   },
 });
