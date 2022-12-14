003483845af3d5a52e9c3ad04dab4a601f2de423
Fauzul Ashari <p4ujul@gmail.com>
2022-12-14 18:42:31 +0700

    Removed id from props.

--- src/components/Dashboard/Anggota/AnggotaList.jsx
+++ src/components/Dashboard/Anggota/AnggotaList.jsx
@@ -2,7 +2,7 @@ import { EditIcon, DeleteIcon } from "../../../assets/icons";
 import { BiCheckboxSquare } from "react-icons/bi";
 import { Link } from "react-router-dom";
 
-const AnggotaList = ({ id, name, phone, email, address }) => {
+const AnggotaList = ({ name, phone, email, address }) => {
   return (
     <tbody className="font-avenirHeavy text-web-dark border-t">
       <tr>
