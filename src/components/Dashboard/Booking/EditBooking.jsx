7512e2421211b2e9cbe4f0166b3310a0e0ef23dd
M. Mustakim Surya <muhammadmustakimsurya@gmail.com>
2022-12-16 20:02:30 +0700

    edit booking (transaksi)

--- src/components/Dashboard/Booking/EditBooking.jsx
+++ src/components/Dashboard/Booking/EditBooking.jsx
@@ -83,56 +83,60 @@ const EditBooking = () => {
                 type="text"
                 className="w-[865px] h-12 ml-12 mb-2 border rounded-lg p-2"
               >
-                <option value="">-- Pilih Metode Pembayaran --</option>
+                <option value="">-- Pilih Status Pembayaran --</option>
                 <option value="BERHASIL">BERHASIL</option>
                 <option value="GAGAL">GAGAL</option>
                 <option value="TERTUNDA">TERTUNDA</option>
               </select>
             </div>
           </div>
-          <div className="flex w-full h-12 justify-start pl-[118px] items-center font-avenirHeavy mb-2 bg-primary-100">
+          <div className="flex w-full h-12 justify-start pl-[118px] items-center font-avenirBlack mb-2 bg-primary-100">
             <label htmlFor="status">Detail Kelas</label>
           </div>
           <div className='flex flex-row'>
             <div>
               <div className="flex w-52 h-12 justify-end items-center font-avenirHeavy mb-2">
-                <label htmlFor="status">Jenis Kelas</label>
+                <label htmlFor="type_kelas">Jenis Kelas</label>
               </div>
               <div className="flex w-52 h-12 justify-end items-center font-avenirHeavy mb-2">
-                <label htmlFor="status">Nama Kelas</label>
+                <label htmlFor="name_kelas">Nama Kelas</label>
               </div>
               <div className="flex w-52 h-12 justify-end items-center font-avenirHeavy mb-2">
-                <label htmlFor="status">Harga Kelas</label>
+                <label htmlFor="price_kelas">Harga Kelas</label>
               </div>
             </div>
             <div>
             <select
-                id="method"
+                id="type_kelas"
                 type="text"
                 className="w-[865px] h-12 ml-12 mb-2 border rounded-lg p-2"
               >
-                <option value="">-- Pilih Metode Pembayaran --</option>
-                <option value="BRI">BRI</option>
-                <option value="BNI">BNI</option>
-                <option value="MANDIRI">MANDIRI</option>
-                <option value="BCA">BCA</option>
-                <option value="QRIS">QRIS</option>
+                <option value="">-- Pilih Jenis Kelas --</option>
+                <option value="Online">Online</option>
+                <option value="Offline">Offline</option>
               </select>
               <select
-                id="method"
+                id="name_kelas"
                 type="text"
                 className="w-[865px] h-12 ml-12 mb-2 border rounded-lg p-2"
               >
-                <option value="">-- Pilih Metode Pembayaran --</option>
-                <option value="BRI">BRI</option>
-                <option value="BNI">BNI</option>
-                <option value="MANDIRI">MANDIRI</option>
-                <option value="BCA">BCA</option>
-                <option value="QRIS">QRIS</option>
+                <option value="">-- Pilih Kelas --</option>
+                <option value="yoga">Yoga untuk Pemula</option>
+                <option value="keseimbangan">Keseimbangan Tubuh</option>
+                <option value="tidur">Tidur Lebih Baik</option>
+                <option value="pompa">Pompa Tubuh</option>
+                <option value="perut">Perut Six Pack</option>
+                <option value="kardio">Langkah Kardio</option>
+                <option value="body">Body Combat</option>
+                <option value="aerobik">Aerobik Energy</option>
+                <option value="zumba">Zumba</option>
+                <option value="muay">Muay Thai</option>
+                <option value="pound">Pound Fit</option>
+                <option value="anti">Antigravity Yoga</option>
               </select>
               <input
-                id="promo"
-                type="text"
+                id="price_kelas"
+                type="number"
                 className="w-[865px] h-12 ml-12 mb-2 border rounded-lg p-2"
               />
             </div>
