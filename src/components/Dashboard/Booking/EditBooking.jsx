b468a33057939d7dfecbb0b7332f9e9bea1c4699
Fauzul Ashari <p4ujul@gmail.com>
2022-12-17 04:06:49 +0700

    Added navigation to button.

--- src/components/Dashboard/Booking/EditBooking.jsx
+++ src/components/Dashboard/Booking/EditBooking.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const EditBooking = () => {
   return (
@@ -92,7 +93,7 @@ const EditBooking = () => {
           <div className="flex w-full h-12 justify-start pl-[118px] items-center font-avenirBlack mb-2 bg-primary-100">
             <label htmlFor="status">Detail Kelas</label>
           </div>
-          <div className='flex flex-row'>
+          <div className="flex flex-row">
             <div>
               <div className="flex w-52 h-12 justify-end items-center font-avenirHeavy mb-2">
                 <label htmlFor="type_kelas">Jenis Kelas</label>
@@ -143,9 +144,11 @@ const EditBooking = () => {
 
           {/* Submit Button */}
           <div className="mt-52 flex justify-end">
-            <button className="w-28 h-14 bg-white text-primary-500 font-avenirBlack rounded-lg mr-4 border border-primary-500 shadow-md">
-              Batal
-            </button>
+            <Link to="/booking">
+              <button className="w-28 h-14 bg-white text-primary-500 font-avenirBlack rounded-lg mr-4 border border-primary-500 shadow-md">
+                Batal
+              </button>
+            </Link>
             <button className="w-52 h-14 bg-primary-500 text-white font-avenirBlack rounded-lg shadow-md">
               Simpan Perubahan
             </button>
@@ -153,7 +156,7 @@ const EditBooking = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default EditBooking
+export default EditBooking;
