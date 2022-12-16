a4479650405223e86e8e89cf429f2ea6ab611066
Fauzul Ashari <p4ujul@gmail.com>
2022-12-17 04:05:53 +0700

    Added PUT Request logic.

--- src/components/Dashboard/Anggota/EditAnggota.jsx
+++ src/components/Dashboard/Anggota/EditAnggota.jsx
@@ -1,6 +1,55 @@
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchDatas } from "../../../redux/gymSlice";
+
+import { Link, useNavigate } from "react-router-dom";
+import APIClient from "../../../apis/APIClient";
+
 const EditAnggota = () => {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state.gym.edit);
+  const navigate = useNavigate();
+
+  const baseData = {
+    name: state.name,
+    email: state.email,
+    phone: state.phone,
+    birth_date: state.birth_date,
+    gender: state.gender,
+    address: state.address,
+  };
+  const [data, setData] = useState(baseData);
+
+  useEffect(() => {
+    console.log(data);
+    setData(baseData);
+  }, []);
+
+  const handleEdit = (e) => {
+    setData((data) => {
+      return { ...data, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    if (!data.name || !data.email || !data.phone || !data.birth_date) {
+      alert("Data tidak boleh kosong");
+    } else {
+      try {
+        e.preventDefault();
+        // add content-type json & charset=UTF-8 to header
+        await APIClient.put(`/users/${state.id}`, data);
+
+        dispatch(fetchDatas({ url: "/users", state: "users" }));
+        navigate("/anggota");
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
-    <div className="ml-[292px] pt-[124px] mr-9">
+    <form className="ml-[292px] pt-[124px] mr-9" onSubmit={handleSubmit}>
       <div>
         <h1 className="font-avenirBlack text-black text-[40px]">
           PERUBAHAN DATA ANGGOTA
@@ -38,36 +87,54 @@ const EditAnggota = () => {
               <input
                 id="name"
                 type="text"
+                name="name"
                 className="w-[523px] h-12 ml-12 mb-2 border rounded-lg p-2"
+                value={data.name}
+                onChange={handleEdit}
               />
               <input
                 id="email"
                 type="email"
+                name="email"
                 className="w-[523px] h-12 ml-12 mb-2 border rounded-lg p-2"
+                value={data.email}
+                onChange={handleEdit}
               />
               <input
                 id="phone"
                 type="text"
+                name="phone"
                 className="w-[523px] h-12 ml-12 mb-2 border rounded-lg p-2"
+                value={data.phone}
+                onChange={handleEdit}
               />
               <input
                 id="ttl"
                 type="date"
+                name="birth_date"
                 className="w-[523px] h-12 ml-12 mb-2 border rounded-lg p-2"
+                value={data.birth_date}
+                onChange={handleEdit}
               />
               <select
                 id="jk"
                 type="text"
+                name="gender"
                 className="w-[523px] h-12 ml-12 mb-2 border rounded-lg p-2"
+                value={data.gender}
+                onChange={handleEdit}
               >
-                <option value="">-- Pilih Jenis Kelamin --</option>
-                <option value="Laki-laki">Laki-laki</option>
+                <option value="Pilih">-- Pilih Jenis Kelamin --</option>
+                <option value={"Laki-laki"}>Laki-laki</option>
                 <option value="Perempuan">Perempuan</option>
               </select>
               <input
                 id="address"
                 type="text"
+                name="address"
                 className="w-[523px] h-12 ml-12 mb-2 border rounded-lg p-2"
+                value={data.address}
+                onChange={handleEdit}
               />
             </div>
 
@@ -86,16 +153,21 @@ const EditAnggota = () => {
 
           {/* Submit Button */}
           <div className="mt-52 flex justify-end">
-            <button className="w-28 h-14 bg-white text-primary-500 font-avenirBlack rounded-lg mr-4 border border-primary-500 shadow-md">
-              Batal
-            </button>
-            <button className="w-52 h-14 bg-primary-500 text-white font-avenirBlack rounded-lg shadow-md">
+            <Link to="/anggota">
+              <button className="w-28 h-14 bg-white text-primary-500 font-avenirBlack rounded-lg mr-4 border border-primary-500 shadow-md">
+                Batal
+              </button>
+            </Link>
+            <button
+              className="w-52 h-14 bg-primary-500 text-white font-avenirBlack rounded-lg shadow-md"
+              type="submit"
+            >
               Simpan Perubahan
             </button>
           </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
