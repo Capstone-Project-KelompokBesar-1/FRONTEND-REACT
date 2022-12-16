5a903496676bcd6ec41816b2dcb148666944b5a6
M. Mustakim Surya <muhammadmustakimsurya@gmail.com>
2022-12-16 20:48:58 +0700

    delete unused icon

--- src/components/Dashboard/Kelas/Kelas.jsx
+++ src/components/Dashboard/Kelas/Kelas.jsx
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchDatas, setSearchField } from "../../../redux/gymSlice";
 
-import { TbDownload } from "react-icons/tb";
 import {
   CalenderIcon,
   SearchIcon,
