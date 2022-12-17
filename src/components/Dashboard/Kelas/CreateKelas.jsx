436a77e69846976aa78f7cde6925e955d248a941
M. Mustakim Surya <muhammadmustakimsurya@gmail.com>
2022-12-17 19:35:59 +0700

    create kelas

--- src/components/Dashboard/Kelas/CreateKelas.jsx
+++ src/components/Dashboard/Kelas/CreateKelas.jsx
@@ -1,10 +1,72 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchDatas } from "../../../redux/gymSlice";
+import APIClient from "../../../apis/APIClient";
+
+import { Link, useNavigate } from "react-router-dom";
 import { TambahDataRed } from "../../../assets/icons";
 
 const CreateKelas = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const baseData = {
+    name: "",
+    type: "",
+    category_id: "",
+    price: "",
+    description: "",
+    total_meeting: "",
+    thumbnail: "https://",
+  };
+  const [data, setData] = useState(baseData);
+
+  useEffect(() => {
+    console.log(data);
+    setData(baseData);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleEdit = (e) => {
+    setData((data) => {
+      return { ...data, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleNumberEdit = (e) => {
+    setData((data) => {
+      return { ...data, [e.target.name]: parseInt(e.target.value) };
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    if (
+      !data.name ||
+      !data.type ||
+      !data.category_id ||
+      !data.price ||
+      !data.description ||
+      !data.total_meeting ||
+      !data.thumbnail
+    ) {
+      alert("Data tidak boleh kosong");
+    } else {
+      try {
+        e.preventDefault();
+        // add content-type json & charset=UTF-8 to header
+        await APIClient.post(`/classes`, data);
+
+        dispatch(fetchDatas({ url: "/classes", state: "classes" }));
+        navigate("/kelas");
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+  console.log(data);
+
   return (
-    <div className="ml-[292px] pt-[124px] mr-9">
+    <form className="ml-[292px] pt-[124px] mr-9" onSubmit={handleSubmit}>
       <div>
         <h1 className="font-avenirBlack text-black text-[40px]">
           TAMBAH KELAS BARU
@@ -45,41 +107,75 @@ const CreateKelas = () => {
               <input
                 id="name"
                 type="text"
+                name="name"
                 className="w-[523px] h-12 ml-12 mb-2 border rounded-lg p-2"
+                value={data.name}
+                onChange={handleEdit}
               />
               <select
                 id="type"
                 type="text"
+                name="type"
                 className="w-[523px] h-12 ml-12 mb-2 border rounded-lg p-2"
+                value={data.type}
+                onChange={handleEdit}
               >
                 <option value="">-- Pilih Jenis Kelas --</option>
-                <option value="Online">Online</option>
-                <option value="Offline">Offline</option>
+                <option value="online">Online</option>
+                <option value="offline">Offline</option>
               </select>
-              <input
-                id="category"
-                type="text"
+
+              <select
+                id="type"
+                type="number"
+                name="category_id"
                 className="w-[523px] h-12 ml-12 mb-2 border rounded-lg p-2"
-              />
+                alue={data.category_id}
+                onChange={handleNumberEdit}
+              >
+                <option value="">-- Pilih Kategori Kelas --</option>
+                <option value={1}>Yoga</option>
+                <option value={2}>Atletik</option>
+              </select>
+
               <input
                 id="price"
-                type="number"
+                type="text"
+                pattern="[0-9]*"
+                name="price"
                 className="w-[523px] h-12 ml-12 mb-2 border rounded-lg p-2"
+                value={data.price}
+                onChange={handleNumberEdit}
               />
               <textarea
                 id="description"
+                type="text"
+                name="description"
                 className="w-[523px] h-[145px] ml-12 mb-2 border rounded-lg p-2 resize-none"
+                value={data.description}
+                onChange={handleEdit}
               />
               <input
                 id="meet"
                 type="number"
+                name="total_meeting"
                 className="w-[523px] h-12 ml-12 mb-2 border rounded-lg p-2"
+                value={data.total_meeting}
+                onChange={handleNumberEdit}
               />
-              <input
+
+              <select
                 id="trainer"
-                type="text"
+                name="trainer_id"
                 className="w-[523px] h-12 ml-12 mb-2 border rounded-lg p-2"
-              />
+                value={data.gender}
+                onChange={handleNumberEdit}
+              >
+                <option value="">-- Pilih Trainer --</option>
+                <option value={1}>Randi</option>
+                <option value={2}>Rian</option>
+                <option value={3}>Milla</option>
+              </select>
             </div>
 
             <div className="flex flex-col ml-10">
@@ -100,13 +196,16 @@ const CreateKelas = () => {
                 Batal
               </button>
             </Link>
-            <button className="w-52 h-14 bg-primary-500 text-white font-avenirBlack rounded-lg shadow-md">
+            <button
+              className="w-52 h-14 bg-primary-500 text-white font-avenirBlack rounded-lg shadow-md"
+              type="submit"
+            >
               Simpan Perubahan
             </button>
           </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
